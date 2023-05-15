2d1fef5e9f62ea0ddb51d448aac93b12332b874c
luizfilho0205 <luizcarlos.0205@gmail.com>
2023-05-14 21:50:20 -0300

    Create TopAppBar component

---
+++ src/components/TopAppBar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TopAppBar() {
+  return (
+    <div className="w-full h-[64px] absolute top-0 right-0 left-0 bg-slate-300 dark:bg-gray-700">
+      <div className="flex flex-row items-center">TOP APP BAR</div>
+    </div>
+  )
+}

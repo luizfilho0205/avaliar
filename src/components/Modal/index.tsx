264ea0260249f6e252a54dded7d543a4369f30df
luizfilho0205 <luizcarlos.0205@gmail.com>
2023-05-14 21:44:45 -0300

    Make it take up the whole screen

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -10,7 +10,9 @@ export default function Modal({ open = false, children }: ModalProps) {
   return (
     <div
       className={
-        open ? 'w-[100vw] h-[100vh] backdrop-blur-sm' : 'hidden invisible'
+        open
+          ? 'absolute top-0 right-0 bottom-0 left-0 z-[999] w-[100vw] h-[100vh] backdrop-blur-sm'
+          : 'hidden invisible'
       }
     >
       {children}
